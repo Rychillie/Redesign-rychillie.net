503020a328636f61526991bbc0d669c058eb9c6d
Rychillie <rychillie@hotmail.com>
2020-08-05 22:41:36 -0300

    removing links

--- src/components/Apresentation/index.js
+++ src/components/Apresentation/index.js
@@ -10,7 +10,7 @@ const Apresentation = () => {
         <S.Wrapper>
             <S.Side>
                 <T.Title>Hey, my name is Rychillie Umpierre de Oliveira and I'm a Brazilian FrontEnd Developer and Content Creator.</T.Title>
-                <T.Text>A young Frontend developer who wants to share his knowledge. Always looking to evolve and improve in all aspects. You can see <S.MyLink to="/portfolio/" title="Portfolio">my work</S.MyLink> or learn more <S.MyLink to="/about/" title="About">about me</S.MyLink>.</T.Text>
+                <T.Text>A young Frontend developer who wants to share his knowledge. Always looking to evolve and improve in all aspects. You can see <S.MyLink to="/" title="Portfolio">my work</S.MyLink> or learn more <S.MyLink to="/" title="About">about me</S.MyLink>.</T.Text>
             </S.Side>
             <Avatar />
         </S.Wrapper>
